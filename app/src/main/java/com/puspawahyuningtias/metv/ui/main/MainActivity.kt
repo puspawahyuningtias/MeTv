9b70d664da5a8144d36ba94e5a914dc18176b3db
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-04-21 16:53:52 +0700

    Init MeTv

---
+++ app/src/main/java/com/puspawahyuningtias/metv/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.puspawahyuningtias.metv.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.puspawahyuningtias.metv.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
