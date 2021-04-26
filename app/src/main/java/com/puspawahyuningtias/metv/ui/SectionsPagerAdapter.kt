1d8ab1c5edc453094e26655d285d5bc503cbf4f7
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-04-26 10:32:43 +0700

    Menambahkan Tab Layout

---
+++ app/src/main/java/com/puspawahyuningtias/metv/ui/SectionsPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.puspawahyuningtias.metv.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter (activity: AppCompatActivity) : FragmentStateAdapter(activity){
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = MoviesFragment()
+            1 -> fragment = TvShowFragment()
+        }
+        return fragment as Fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+}
--- app/src/main/java/com/puspawahyuningtias/metv/ui/main/MainActivity.kt
+++ app/src/main/java/com/puspawahyuningtias/metv/ui/main/MainActivity.kt
@@ -2,11 +2,31 @@ package com.puspawahyuningtias.metv.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.puspawahyuningtias.metv.R
+import com.puspawahyuningtias.metv.ui.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_movies,
+            R.string.tab_tv_show
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation = 0f
     }
 }
