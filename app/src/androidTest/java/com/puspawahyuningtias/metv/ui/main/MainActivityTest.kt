6825df869f6488e4f18dccdec66f123403086b17
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-05-24 16:51:47 +0700

    penambahan Idling Resouces untuk menangani asynchronus proses saat Instrumental Testing

--- app/src/androidTest/java/com/puspawahyuningtias/metv/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/puspawahyuningtias/metv/ui/main/MainActivityTest.kt
@@ -1,27 +1,37 @@
 package com.puspawahyuningtias.metv.ui.main
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.puspawahyuningtias.metv.R
 import com.puspawahyuningtias.metv.data.DataFilm
-import org.junit.Rule
+import com.puspawahyuningtias.metv.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 class MainActivityTest {
     private val dataMovie = DataFilm.generateMovies()
     private val dataTvShow = DataFilm.generateTvShow()
 
-    @get:Rule
-    var activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
+    }
 
     @Test
     fun loadMovies() {
-        delayThreeSecond()
         onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_movies)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
@@ -32,7 +42,6 @@ class MainActivityTest {
 
     @Test
     fun loadDetailFilm() {
-        delayThreeSecond()
         onView(withId(R.id.rv_movies)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
@@ -52,7 +61,6 @@ class MainActivityTest {
     @Test
     fun loadTvShow() {
         onView(withText("TV SHOW")).perform(click())
-        delayThreeSecond()
         onView(withId(R.id.rv_tv_show)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_tv_show)).perform(
             RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
@@ -64,7 +72,6 @@ class MainActivityTest {
     @Test
     fun loadDetailTvShow() {
         onView(withText("TV SHOW")).perform(click())
-        delayThreeSecond()
         onView(withId(R.id.rv_tv_show)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
@@ -81,11 +88,4 @@ class MainActivityTest {
         onView(withId(R.id.tv_tvshow_year)).check(matches(withText(dataTvShow[0].year)))
     }
 
-    private fun delayThreeSecond() {
-        try {
-            Thread.sleep(3000)
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
-    }
 }
--- app/src/main/java/com/puspawahyuningtias/metv/ui/Movies/MoviesViewModel.kt
+++ app/src/main/java/com/puspawahyuningtias/metv/ui/Movies/MoviesViewModel.kt
@@ -1,9 +1,7 @@
 package com.puspawahyuningtias.metv.ui.movies
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.puspawahyuningtias.metv.data.DataFilm
 import com.puspawahyuningtias.metv.data.FilmRepository
 import com.puspawahyuningtias.metv.data.Movies
 
