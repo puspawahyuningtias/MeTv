095dd8a0dfa67267ed5d8f830861cc6be1dcd1d0
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-06-02 05:32:46 +0700

    update unit test movies

--- app/src/main/java/com/puspawahyuningtias/metv/data/Movies.kt
+++ app/src/main/java/com/puspawahyuningtias/metv/data/Movies.kt
@@ -1,9 +1,13 @@
 package com.puspawahyuningtias.metv.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Movies(
     var title: String,
     val year: String,
     var genre: String,
     var description: String,
     var photo: Int
-)
+) : Parcelable
--- app/src/test/java/com/puspawahyuningtias/metv/ui/movies/detail/DetailMoviesViewModelTest.kt
+++ app/src/test/java/com/puspawahyuningtias/metv/ui/movies/detail/DetailMoviesViewModelTest.kt
@@ -1,31 +1,62 @@
 package com.puspawahyuningtias.metv.ui.movies.detail
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.puspawahyuningtias.metv.data.DataFilm
+import com.puspawahyuningtias.metv.data.FilmRepository
+import com.puspawahyuningtias.metv.data.Movies
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class DetailMoviesViewModelTest {
     private lateinit var viewModel: DetailMoviesViewModel
-    private val dataMovie = DataFilm.generateMovies()[0]
-    private val title = dataMovie.title
+    private val dataMovies = DataFilm.generateMovies()[0]
+    private val title = dataMovies.title
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var filmRepository: FilmRepository
+
+    @Mock
+    private lateinit var observer: Observer<List<Movies>>
 
     @Before
     fun setUp() {
-        viewModel = DetailMoviesViewModel()
+        viewModel = DetailMoviesViewModel(filmRepository)
         viewModel.setSelectedMovies(title)
     }
 
     @Test
     fun getMovies() {
-        viewModel.setSelectedMovies(dataMovie.title)
-        val movies = viewModel.getMovies()
-        assertNotNull(movies)
-        assertEquals(dataMovie.title, movies.title)
-        assertEquals(dataMovie.description, movies.description)
-        assertEquals(dataMovie.photo, movies.photo)
-        assertEquals(dataMovie.year, movies.year)
-        assertEquals(dataMovie.genre, movies.genre)
+        viewModel.setSelectedMovies(dataMovies.title)
+
+        val dummyMovies = DataFilm.generateMovies()
+        val movies = MutableLiveData<List<Movies>>()
+        movies.value = dummyMovies
+
+        `when`(filmRepository.getMovies()).thenReturn(movies)
+        val courseEntity = viewModel.getMovies().value
+        verify(filmRepository).getMovies()
+        assertNotNull(courseEntity)
+        assertEquals(dataMovies.title, courseEntity?.get(0)?.title)
+        assertEquals(dataMovies.description, courseEntity?.get(0)?.description)
+        assertEquals(dataMovies.photo, courseEntity?.get(0)?.photo)
+        assertEquals(dataMovies.year, courseEntity?.get(0)?.year)
+        assertEquals(dataMovies.genre, courseEntity?.get(0)?.genre)
+
+        viewModel.getMovies().observeForever(observer)
+        verify(observer).onChanged((dummyMovies))
     }
 }
